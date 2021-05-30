6f6096390b66bca9999b647f24dc0347c942d8e6
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-05-30 15:32:55 +0200

    footer content and style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="text-center text-lg-start text-white">
     <!-- Section: Social media -->
-    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    {{-- <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
         <!-- Left -->
         <div class="me-5 d-none d-lg-block">
             <span>Get connected with us on social networks:</span>
@@ -30,12 +30,12 @@
             </a>
         </div>
         <!-- Right -->
-    </section>
+    </section> --}}
     <!-- Section: Social media -->
 
     <!-- Section: Links  -->
-    <section class="">
-        <div class="container text-center text-md-start mt-5">
+    <section>
+        <div class="container text-center text-md-start pt-4">
             <!-- Grid row -->
             <div class="row mt-3">
                 <!-- Grid column -->
@@ -45,8 +45,10 @@
                         {{ __('Eurazijos agro') }}
                     </h6>
                     <p>
-                        Here you can use rows and columns to organize your footer content. Lorem ipsum
-                        dolor sit amet, consectetur adipisicing elit.
+                        {{ __('For those, looking for the best solutions of their gardens and yards') }}.
+                    </p>
+                    <p>
+                        {{ __('Wholesale in bulk quantities from 150 units') }}.
                     </p>
                 </div>
                 <!-- Grid column -->
@@ -58,16 +60,20 @@
                         {{ __('pages') }}
                     </h6>
                     <p>
-                        <a href="{{ route('home', app()->getLocale()) }}" class="text-reset text-capitalize">{{ __('home') }}</a>
+                        <a href="{{ route('home', app()->getLocale()) }}"
+                            class="text-reset text-capitalize">{{ __('home') }}</a>
                     </p>
                     <p>
-                        <a href="{{ route('catalog', app()->getLocale()) }}" class="text-reset text-capitalize">{{ __('catalog') }}</a>
+                        <a href="{{ route('catalog', app()->getLocale()) }}"
+                            class="text-reset text-capitalize">{{ __('catalog') }}</a>
                     </p>
                     <p>
-                        <a href="{{ route('about', app()->getLocale()) }}" class="text-reset text-capitalize">{{ __('about') }}</a>
+                        <a href="{{ route('about', app()->getLocale()) }}"
+                            class="text-reset text-capitalize">{{ __('about') }}</a>
                     </p>
                     <p>
-                        <a href="{{ route('contact', app()->getLocale()) }}" class="text-reset text-capitalize">{{ __('contact') }}</a>
+                        <a href="{{ route('contact', app()->getLocale()) }}"
+                            class="text-reset text-capitalize">{{ __('contact') }}</a>
                     </p>
                     <p>
                         <a href="#!" class="text-reset text-capitalize">{{ __('privacy policy') }}</a>
@@ -76,18 +82,33 @@
                 <!-- Grid column -->
 
                 <!-- Grid column -->
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <div class="col-md-6 col-lg-6 col-xl-7 mx-auto mb-md-0 mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
                         {{ __('contact') }}
                     </h6>
-                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-                    <p>
-                        <i class="fas fa-envelope me-3"></i>
-                        info@example.com
-                    </p>
-                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                <i class="fas fa-envelope me-3"></i>
+                                eurazijos@mail.ru
+                            </p>
+                            <p><i class="fas fa-phone me-3"></i> +370 60780165</p>
+                            <p><i class="fas fa-phone me-3"></i>+370 67129929</p>
+                            <p><i class="fas fa-home me-3"></i> Ukmerge Vilniaus g. 100B-45, LT-20166. Lithuania</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>
+                                <i class="fas fa-envelope me-3"></i>
+                                bthtatiana@gmail.com
+                            </p>
+                            <p><i class="fas fa-phone me-3"></i> +375 296399939</p>
+                            <p><i class="fas fa-phone me-3"></i>+375 (33) 395-49-10</p>
+                            <p><i
+                                    class="fas fa-home me-3"></i>{{ __('18b Dzerzhinskogo St., Tsnyanka, Minsk district. Belarus') }}
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <!-- Grid column -->
             </div>

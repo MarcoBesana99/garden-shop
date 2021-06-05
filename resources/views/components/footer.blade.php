b8be14d77d8ce21fc5cd952f5140b1b2680f01e8
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-06-05 19:41:16 +0200

    fix copyright section footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -120,7 +120,7 @@
     <!-- Copyright -->
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
         © 2021 Copyright:
-        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        <a class="text-reset fw-bold" href="https://eurazijos-agro.com/">eurazijos-agro.com</a>
     </div>
     <!-- Copyright -->
 </footer>
